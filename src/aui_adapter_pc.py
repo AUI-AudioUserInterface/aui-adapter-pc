c7d44b855071f1d4b3336326a42e968a8b20096c
CoPiCo2Co <CoPiCo2Co@googlemail.com>
2025-09-25 16:39:00 +0200

    WIP

--- src/aui_adapter_pc.py
+++ src/aui_adapter_pc.py
@@ -1,3 +1,7 @@
+from typing import Optional, Any
+from auicommon.adapter.meta import AdapterMeta
+from auicommon.audio.types import PcmAudio
+from auicommon.util.async_utils import CancellationToken  # bei dir vorhanden
 
 DTMF_CHARS = set("0123456789*#")
 
@@ -6,11 +10,25 @@ class PcAdapter:
         self._name = name
         self._busy = False
 
+    def meta(self) -> AdapterMeta:
+        return AdapterMeta(name='pc', version='0.0.1', vendor="CoPiCo2Co", description='')
+    """Abstraktion für Audio-Ausgabe/Transporte (z. B. PC, ARI, SIP…)."""
+
+    async def init(self, **kwargs: Any) -> None:
+        return None
+
     async def start(self) -> None:
-        pass
+        return None
 
     async def stop(self) -> None:
-        pass
+        return None
 
-    def is_busy(self) -> bool:
-        return False
+    async def play(self, audio: PcmAudio, *, wait: bool = False,
+                   cancel: Optional[CancellationToken] = None) -> None:
+        return None
+    
+    """Audio wiedergeben. Falls `wait=True`, erst zurückkehren, wenn Ausgabe beendet."""
+
+    async def stop_audio(self) -> None:
+        return None
+    """Laufende Audioausgabe umgehend abbrechen (Not-Aus)."""
